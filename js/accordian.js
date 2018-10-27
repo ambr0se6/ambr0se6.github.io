3e9d46b9248d3b7e4db720c0904c2443f9bbfb5a
Ryan Burgett <thesonicdoctor@gmail.com>
2018-10-27 15:20:34 -0400

    Completed Assignment 5

--- js/accordian.js
+++ js/accordian.js
@@ -3,6 +3,12 @@ const accordians = document.querySelectorAll(".accordion a");
 function toggleAccordion(){
     this.classList.toggle('active');
     this.nextElementSibling.classList.toggle('active');
+    const activeAccordians = document.querySelectorAll(".accordian a.active");
+    activeAccordians
+        .filter(a => a !== this)
+        .forEach(a => {
+            toggleAccordion
+        });
 }
 
 accordians.forEach(item => item.addEventListener('click', toggleAccordion));
